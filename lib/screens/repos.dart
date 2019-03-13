0b57a59392df9a341a67edb811f9dd57b5b2a724
Rongjian Zhang <pd4d10@gmail.com>
2019-03-13 22:04:59 +0800

    fix: show repo owner at user stars screen

--- lib/screens/repos.dart
+++ lib/screens/repos.dart
@@ -63,7 +63,7 @@ class _ReposScreenState extends State<ReposScreen> {
       title: Text('$login\'s $title'),
       onRefresh: () => _queryRepos(),
       onLoadMore: (cursor) => _queryRepos(cursor),
-      itemBuilder: (payload) => RepoItem(payload, showOwner: false),
+      itemBuilder: (payload) => RepoItem(payload, showOwner: widget.star),
     );
   }
 }
