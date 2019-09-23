d57389934cc3877bdf1b20d78623058f8e6dadb4
Rongjian Zhang <pd4d10@gmail.com>
2019-09-23 20:59:34 +0800

    build: flutter create

---
+++ android/app/src/main/kotlin/io/github/pd4d10/gittouch/MainActivity.kt
@@ -0,0 +1,13 @@
+package io.github.pd4d10.gittouch
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
