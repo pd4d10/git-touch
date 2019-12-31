cdd8876657b721dfcbc9063a30e42cd7b53a905a
Rongjian Zhang <pd4d10@gmail.com>
2019-12-31 19:52:00 +0800

    improvement: drop inkwell widget in cupertino style

--- lib/widgets/link.dart
+++ lib/widgets/link.dart
@@ -3,6 +3,50 @@ import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 import 'package:git_touch/models/theme.dart';
 
+// TODO:
+class CupertinoLink extends StatefulWidget {
+  final Widget child;
+  final Function onTap;
+
+  CupertinoLink({this.child, this.onTap});
+
+  @override
+  _CupertinoLinkState createState() => _CupertinoLinkState();
+}
+
+class _CupertinoLinkState extends State<CupertinoLink> {
+  Color _color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: _color,
+      child: GestureDetector(
+        onTap: widget.onTap,
+        onTapDown: (_) {
+          print('down');
+          setState(() {
+            _color = Colors.black12;
+          });
+        },
+        onTapUp: (_) {
+          print('up');
+          setState(() {
+            _color = null;
+          });
+        },
+        onTapCancel: () {
+          print('cacnel');
+          setState(() {
+            _color = null;
+          });
+        },
+        child: widget.child,
+      ),
+    );
+  }
+}
+
 class Link extends StatelessWidget {
   final Widget child;
   final String url;
@@ -18,19 +62,24 @@ class Link extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Provider.of<ThemeModel>(context);
 
-    return Material(
-      child: Ink(
-        color: CupertinoTheme.of(context).scaffoldBackgroundColor,
-        child: InkWell(
+    switch (theme.theme) {
+      case AppThemeType.cupertino:
+        return CupertinoButton(
           child: child,
-          splashColor:
-              theme.theme == AppThemeType.cupertino ? Colors.transparent : null,
+          padding: EdgeInsets.all(0),
+          onPressed: () async {
+            if (onTap != null) onTap();
+            theme.push(context, url);
+          },
+        );
+      default:
+        return InkWell(
+          child: child,
           onTap: () async {
             if (onTap != null) onTap();
             theme.push(context, url);
           },
-        ),
-      ),
-    );
+        );
+    }
   }
 }
