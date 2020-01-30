f604d467dcee36954268a4cce3eefea79704141e
Rongjian Zhang <pd4d10@gmail.com>
2020-01-30 14:51:22 +0800

    improvement(gitlab): code settings

--- lib/screens/gitlab_blob.dart
+++ lib/screens/gitlab_blob.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:git_touch/models/auth.dart';
 import 'package:git_touch/models/gitlab.dart';
+import 'package:git_touch/models/theme.dart';
 import 'package:git_touch/scaffolds/refresh_stateful.dart';
 import 'package:git_touch/utils/utils.dart';
+import 'package:git_touch/widgets/action_entry.dart';
 import 'package:git_touch/widgets/app_bar_title.dart';
 import 'package:git_touch/widgets/blob_view.dart';
 import 'package:provider/provider.dart';
@@ -28,6 +30,13 @@ class GitlabBlobScreen extends StatelessWidget {
             '/projects/$id/repository/files/$encodedPath?ref=master'); // TODO:
         return GitlabBlob.fromJson(res);
       },
+      action: ActionEntry(
+        iconData: Icons.settings,
+        onTap: () {
+          final theme = Provider.of<ThemeModel>(context);
+          theme.push(context, '/choose-code-theme');
+        },
+      ),
       bodyBuilder: (data, _) {
         return BlobView(path, base64Text: data.content);
       },
