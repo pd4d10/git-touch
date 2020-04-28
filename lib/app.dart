d343340d50bb6a66852468d46228836c8522e9f8
shreyas1599 <shreyasthirumalai@gmail.com>
2020-04-28 23:59:08 +0530

    fix: transparent status bar

--- lib/app.dart
+++ lib/app.dart
@@ -4,6 +4,7 @@ import 'package:git_touch/home.dart';
 import 'package:git_touch/models/auth.dart';
 import 'package:git_touch/models/theme.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/services.dart';
 
 class MyApp extends StatelessWidget {
   Widget _buildChild(BuildContext context) {
@@ -31,6 +32,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final auth = Provider.of<AuthModel>(context);
+    // To match status bar color to app bar color
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ));
     return Container(key: auth.rootKey, child: _buildChild(context));
   }
 }
