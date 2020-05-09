e17371386143824f2c108942d6fb09102f4616d9
Shreyas Thirumalai <shreyasthirumalai@gmail.com>
2020-05-09 12:13:56 +0530

    fix: error using same token  (#83)
    Fixes #82

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -148,8 +148,14 @@ class AuthModel with ChangeNotifier {
       if (info['message'] != null) {
         throw info['message'];
       }
+      if (info['error'] != null) {
+        throw info['error'] +
+            '. ' +
+            (info['error_description'] != null
+                ? info['error_description']
+                : '');
+      }
       final user = GitlabUser.fromJson(info);
-
       await _addAccount(Account(
         platform: PlatformType.gitlab,
         domain: domain,
