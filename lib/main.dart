442d910dbdcdc9d1dae519c6e44085874ddf2f76
Rongjian Zhang <pd4d10@gmail.com>
2021-06-14 14:35:10 +0800

    feat: crash report
    closes #114

--- lib/main.dart
+++ lib/main.dart
@@ -8,80 +8,69 @@ import 'package:git_touch/router.dart';
 import 'package:provider/provider.dart';
 import 'package:git_touch/models/notification.dart';
 import 'package:fluro/fluro.dart';
-import 'package:fimber/fimber.dart';
 import 'package:flutter/services.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  // Platform messages may fail, so we use a try/catch PlatformException.
+  await SentryFlutter.init(
+    (options) {
+      options.dsn =
+          'https://006354525fa244289c48169790fa3757@o71119.ingest.sentry.io/5814819';
+    },
+    // Init your App.
+    appRunner: () async {
+      final notificationModel = NotificationModel();
+      final themeModel = ThemeModel();
+      final authModel = AuthModel();
+      final codeModel = CodeModel();
+      await Future.wait([
+        themeModel.init(),
+        authModel.init(),
+        codeModel.init(),
+      ]);
 
-  // try {
-  //   String initialLink = await getInitialLink();
-  //   Fimber.d(initialLink);
-  // } on PlatformException {
-  //   Fimber.d('test');
-  // }
+      CommonRouter.routes.forEach((screen) {
+        themeModel.router.define(CommonRouter.prefix + screen.path,
+            handler: Handler(handlerFunc: screen.handler));
+      });
+      GitlabRouter.routes.forEach((screen) {
+        themeModel.router.define(GitlabRouter.prefix + screen.path,
+            handler: Handler(handlerFunc: screen.handler));
+      });
+      GiteaRouter.routes.forEach((screen) {
+        themeModel.router.define(GiteaRouter.prefix + screen.path,
+            handler: Handler(handlerFunc: screen.handler));
+      });
+      BitbucketRouter.routes.forEach((screen) {
+        themeModel.router.define(BitbucketRouter.prefix + screen.path,
+            handler: Handler(handlerFunc: screen.handler));
+      });
+      GithubRouter.routes.forEach((screen) {
+        themeModel.router.define(GithubRouter.prefix + screen.path,
+            handler: Handler(handlerFunc: screen.handler));
+      });
+      GiteeRouter.routes.forEach((screen) {
+        themeModel.router.define(GiteeRouter.prefix + screen.path,
+            handler: Handler(handlerFunc: screen.handler));
+      });
+      GogsRouter.routes.forEach((screen) {
+        themeModel.router.define(GogsRouter.prefix + screen.path,
+            handler: Handler(handlerFunc: screen.handler));
+      });
+      // To match status bar color to app bar color
+      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+      ));
 
-  // DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-  // AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
-  // Fimber.d('Running on ${androidInfo.model}'); // e.g. "Moto G (4)"
-
-  // IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
-  // Fimber.d('Running on ${iosInfo.utsname.machine}'); // e.g. "iPod7,1"
-
-  // TODO: Later, should check to enable debug log in debug build only
-  Fimber.plantTree(DebugTree());
-
-  final notificationModel = NotificationModel();
-  final themeModel = ThemeModel();
-  final authModel = AuthModel();
-  final codeModel = CodeModel();
-  await Future.wait([
-    themeModel.init(),
-    authModel.init(),
-    codeModel.init(),
-  ]);
-
-  CommonRouter.routes.forEach((screen) {
-    themeModel.router.define(CommonRouter.prefix + screen.path,
-        handler: Handler(handlerFunc: screen.handler));
-  });
-  GitlabRouter.routes.forEach((screen) {
-    themeModel.router.define(GitlabRouter.prefix + screen.path,
-        handler: Handler(handlerFunc: screen.handler));
-  });
-  GiteaRouter.routes.forEach((screen) {
-    themeModel.router.define(GiteaRouter.prefix + screen.path,
-        handler: Handler(handlerFunc: screen.handler));
-  });
-  BitbucketRouter.routes.forEach((screen) {
-    themeModel.router.define(BitbucketRouter.prefix + screen.path,
-        handler: Handler(handlerFunc: screen.handler));
-  });
-  GithubRouter.routes.forEach((screen) {
-    themeModel.router.define(GithubRouter.prefix + screen.path,
-        handler: Handler(handlerFunc: screen.handler));
-  });
-  GiteeRouter.routes.forEach((screen) {
-    themeModel.router.define(GiteeRouter.prefix + screen.path,
-        handler: Handler(handlerFunc: screen.handler));
-  });
-  GogsRouter.routes.forEach((screen) {
-    themeModel.router.define(GogsRouter.prefix + screen.path,
-        handler: Handler(handlerFunc: screen.handler));
-  });
-  // To match status bar color to app bar color
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-    statusBarColor: Colors.transparent,
-  ));
-
-  runApp(MultiProvider(
-    providers: [
-      ChangeNotifierProvider(create: (context) => notificationModel),
-      ChangeNotifierProvider(create: (context) => themeModel),
-      ChangeNotifierProvider(create: (context) => authModel),
-      ChangeNotifierProvider(create: (context) => codeModel),
-    ],
-    child: MyApp(),
-  ));
+      runApp(MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (context) => notificationModel),
+          ChangeNotifierProvider(create: (context) => themeModel),
+          ChangeNotifierProvider(create: (context) => authModel),
+          ChangeNotifierProvider(create: (context) => codeModel),
+        ],
+        child: MyApp(),
+      ));
+    },
+  );
 }
